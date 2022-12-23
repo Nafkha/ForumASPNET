1d431a0da30c2f056e2da49115b823537c83f719
Mohamed Youssef Nafkha <m.nafkha21224@pi.tn>
2022-12-23 10:59:30 +0100

    Added post listing

--- ProjetCsharp.DAL/Migrations/20221223094844_mig.Designer.cs
+++ ProjetCsharp.DAL/Migrations/20221223094844_mig.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace ProjetCsharp.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221223094844_mig")]
+    partial class mig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
